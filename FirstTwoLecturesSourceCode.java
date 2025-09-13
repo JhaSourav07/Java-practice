00f995519a2f934f07dbff445f6a5aec9054c155
Sourav <souravkjha2007@gmail.com>
2025-09-13 18:13:27 +0530

    Initial commit with Java file

---
+++ FirstTwoLecturesSourceCode.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class FirstTwoLecturesSourceCode {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        byte a = 10;
+        short b = 20;
+        int c = 30;
+        long d = 40L;
+        float e = 50.0f;
+        double f = 60.0;
+        char g = 'A';
+        boolean h = true;
+        String i = "Hello, World!";
+        System.out.println("Byte: " + a);
+        System.out.println("Short: " + b);
+        System.out.println("Int: " + c);
+        System.out.println("Long: " + d);
+        System.out.println("Float: " + e);
+        System.out.println("Double: " + f);
+        System.out.println("Char: " + g);
+        System.out.println("Boolean: " + h);
+        System.out.println("String: " + i);
+    }
+}
